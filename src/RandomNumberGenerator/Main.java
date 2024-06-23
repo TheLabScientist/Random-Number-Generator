8c75e95bf42f208deab9e9e4ef81019bb38e1509
TheLabScientist <110190470+TheLabScientist@users.noreply.github.com>
2024-06-23 15:25:10 -0400

    Fixed formatting

--- src/RandomNumberGenerator/Main.java
+++ src/RandomNumberGenerator/Main.java
@@ -1,8 +1,3 @@
-/* Random Number Generator - Copyright © TheLabScientist 2024
- * With this script, you can easily generate numbers for whatever
- * needs be
- */
-
 package RandomNumberGenerator;
 
 import RandomNumberGenerator.RandomNumber;
@@ -12,9 +7,9 @@ public class Main {
     
     public static void main(String[] args) {
         RandomNumber rn = new RandomNumber(12);
-        rn.allowDuplicates(true); // Can be omitted
-        rn.setMixing(Mix.BOTH); // Mix.BOTH | Mix.EVEN | Mix.ODD
-								    
+        rn.allowDuplicates(true);
+        rn.setMixing(Mix.BOTH);
+								     
         System.out.println(rn.getNumber());
-								 }
+    }
 }
